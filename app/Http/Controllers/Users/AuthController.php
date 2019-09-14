6931838c02ed658056c8fbeb4a629389d995927d
ladapo samuel <ladaposamuel@outlook.com>
2019-09-14 12:34:56 +0100

    ft(user-signup): user should be able to create a new account
    - create route
    - wrote validation
    - create controller function
    - wrote tests to affirm feature
    [Finishes #168444322]

--- app/Http/Controllers/Users/AuthController.php
+++ app/Http/Controllers/Users/AuthController.php
@@ -9,6 +9,12 @@ use Firebase\JWT\ExpiredException;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 
+
+/**
+ * @group Authentication Route
+ *
+ * Auth route
+ */
 class AuthController extends Controller
 {
 
@@ -31,8 +37,16 @@ class AuthController extends Controller
    }
 
    /**
-    * @return \Illuminate\Http\JsonResponse
-    * @throws \Illuminate\Validation\ValidationException
+    * Login Route
+    *
+    * @bodyParam email email required User Email.
+    * @bodyParam password string required User Password.
+    *
+    * @response {
+    *  "status" : "success",
+    *  "data" : {"user" : {}, "token": ""}
+    * }
+    *
     */
    public function login()
    {
@@ -43,21 +57,67 @@ class AuthController extends Controller
       // Find the user by email
       $user = User::where('email', $this->request->input('email'))->first();
       if (!$user) {
-      return respond(400,'error',[
-             'error' => 'Email does not exist.'
-         ]);
+         return respond('error', [
+            'error' => 'Email does not exist.'
+         ], 400);
       }
       // Verify the password and generate the token
       if (Hash::check($this->request->input('password'), $user->password)) {
-      return respond(200,'success',[
+         return respond('success', [
             'user' => $user,
             'token' => $this->jwt($user)
          ]);
       }
       // Bad Request response
-      return respond(400,'error',[
-             'error' => 'Email or password is not correct.'
+      return respond('error', [
+         'error' => 'Email or password is not correct.'
+      ], 400);
+   }
+
+   /**
+    * Register Route
+    *
+    * @bodyParam name string required User Name.
+    * @bodyParam email email required User Email
+    * @bodyParam password string required User Password.
+    *
+    * @response {
+    *  "status" : "success",
+    *  "data" : {"user" : {}, "token": ""}
+    * }
+    *
+    */
+   public function register(Request $request)
+   {
+
+      $validator = \Validator::make($request->all(), [
+         'name' => 'required',
+         'email' => 'required|email|unique:users',
+         'password' => 'required'
+      ]);
+
+      if ($validator->fails()) {
+         return respond('error', $validator->errors(), 422);
+      }
+      try {
+         $email = $request->input('email');
+         $name = $request->input('name');
+         $password = app('hash')->make($request->input('password'));
+         $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
          ]);
+         return respond('success', [
+            'user' => $user,
+            'token' => $this->jwt($user)
+         ]);
+      } catch (\Exception $error) {
+         return respond('error', [
+            'error' => $error
+         ], 422);
+      }
+
    }
 
    protected function jwt(User $user)
--- public/docs/source/.compare.md
+++ public/docs/source/.compare.md
@@ -20,6 +20,149 @@ Welcome to the generated API reference.
 
 <!-- END_INFO -->
 
+#Authentication Route
+
+
+Auth route
+<!-- START_f76cc718539c2362f0d0a7069100319e -->
+## Login Route
+
+> Example request:
+
+```php
+
+$client = new \GuzzleHttp\Client();
+$response = $client->post("http://localhost/api/v1/auth/login", [
+    'headers' => [
+            "Content-Type" => "application/json",
+        ],
+    'json' => [
+            "email" => "delectus",
+            "password" => "voluptatibus",
+        ],
+]);
+$body = $response->getBody();
+print_r(json_decode((string) $body));
+```
+
+```javascript
+const url = new URL("http://localhost/api/v1/auth/login");
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
+
+let body = {
+    "email": "delectus",
+    "password": "voluptatibus"
+}
+
+fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: body
+})
+    .then(response => response.json())
+    .then(json => console.log(json));
+```
+
+
+> Example response (200):
+
+```json
+{
+    "status": "success",
+    "data": {
+        "user": {},
+        "token": ""
+    }
+}
+```
+
+### HTTP Request
+`POST /api/v1/auth/login`
+
+#### Body Parameters
+
+Parameter | Type | Status | Description
+--------- | ------- | ------- | ------- | -----------
+    email | email |  required  | User Email.
+    password | string |  required  | User Password.
+
+<!-- END_f76cc718539c2362f0d0a7069100319e -->
+
+<!-- START_61e12326a1f2070b5637f4366c2ce678 -->
+## Register Route
+
+> Example request:
+
+```php
+
+$client = new \GuzzleHttp\Client();
+$response = $client->post("http://localhost/api/v1/auth/register", [
+    'headers' => [
+            "Content-Type" => "application/json",
+        ],
+    'json' => [
+            "name" => "vitae",
+            "email" => "illum",
+            "password" => "ex",
+        ],
+]);
+$body = $response->getBody();
+print_r(json_decode((string) $body));
+```
+
+```javascript
+const url = new URL("http://localhost/api/v1/auth/register");
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
+
+let body = {
+    "name": "vitae",
+    "email": "illum",
+    "password": "ex"
+}
+
+fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: body
+})
+    .then(response => response.json())
+    .then(json => console.log(json));
+```
+
+
+> Example response (200):
+
+```json
+{
+    "status": "success",
+    "data": {
+        "user": {},
+        "token": ""
+    }
+}
+```
+
+### HTTP Request
+`POST /api/v1/auth/register`
+
+#### Body Parameters
+
+Parameter | Type | Status | Description
+--------- | ------- | ------- | ------- | -----------
+    name | string |  required  | User Name.
+    email | email |  required  | User Email
+    password | string |  required  | User Password.
+
+<!-- END_61e12326a1f2070b5637f4366c2ce678 -->
+
 #Index Route
 
 
